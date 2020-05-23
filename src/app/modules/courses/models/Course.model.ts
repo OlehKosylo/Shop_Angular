90823ff6f39c3792732a27c6169e4c60f5c0467f
Your Name <you@example.com>
2020-05-23 15:04:21 +0300

    demo

--- src/app/modules/courses/models/Course.model.ts
+++ src/app/modules/courses/models/Course.model.ts
@@ -1,8 +1,8 @@
 export interface CourseModel {
   id?: number;
   title: string;
-  description: string;
-  price: number;
+  description?: string;
+  price?: number;
   publicKey?: string;
   genre?: string;
   downloadURL?: string;
